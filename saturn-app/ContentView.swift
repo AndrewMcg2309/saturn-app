25d7a504f414f547bfc75c3c001fd0731a8c2ebd
Andrew McGowan <andrew.mcgowan@immedis.com>
2024-04-21 19:21:14 +0100

    update stop instance

--- saturn-app/ContentView.swift
+++ saturn-app/ContentView.swift
@@ -8,22 +8,54 @@
 import SwiftUI
 import UIKit
 
+
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Saturn App")
+            Text("AWS Instance")
+                .bold()
+                .padding()
+            
+            Text("This app uses HTTP requests to spin up and shut down the AWS Instance that runs the online portfolio")
+                
             
             Button(action: AwsStart) {
-                Text("Start Instance")
+                Text("Start")
             }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color(red: 50/255, green: 200/255, blue: 80/255))
+            .clipShape(Capsule())
+            
+            Button(action: AwsStop) {
+                Text("Stop")
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color(red: 200/255, green: 33/255, blue: 1/255))
+            .clipShape(Capsule())
+            .padding()
+            
+            Button(action: AwsStatus) {
+                Text("Status")
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color(red: 50/255, green: 100/255, blue: 200/255))
+            .clipShape(Capsule())
+            
+            Text("---------------------------")
+                .padding()
         }
         .padding()
     }
 }
 
+
 #Preview {
     ContentView()
 }
+
--- saturn-app/saturn_appApp.swift
+++ saturn-app/saturn_appApp.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 
-
 @main
 struct saturn_appApp: App {
     var body: some Scene {
@@ -20,7 +19,7 @@ struct saturn_appApp: App {
 func AwsStart(){
     let url_start = URL(string: "https://fyrbzl2e5j.execute-api.eu-west-1.amazonaws.com/startWebsite")!
     let message = Message(
-        message: "Request from iPhone"
+        message: "Start request from iPhone"
     )
     struct Message: Encodable {
         let message: String
@@ -40,10 +39,45 @@ func AwsStart(){
 
         if statusCode == 200 {
             print("Succesful POST")
-            
+
         } else {
             print("POST failed")
         }
     }
     task.resume()
+}
+
+func AwsStop(){
+    let url_start = URL(string: "https://fyrbzl2e5j.execute-api.eu-west-1.amazonaws.com/stopWebsite")!
+    let message = Message(
+        message: "Stop request from iPhone"
+    )
+    struct Message: Encodable {
+        let message: String
+    }
+    let data = try! JSONEncoder().encode(message)
+    
+    var request = URLRequest(url: url_start)
+    request.httpMethod = "POST"
+    request.httpBody = data
+    request.setValue(
+        "application/json",
+        forHTTPHeaderField: "Content-Type"
+    )
+    
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        let statusCode = (response as! HTTPURLResponse).statusCode
+
+        if statusCode == 200 {
+            print("Succesful POST")
+
+        } else {
+            print("POST failed")
+        }
+    }
+    task.resume()
+}
+
+func AwsStatus(){
+    
 }
